122f04a8a8ce59239849d953956391130b1b80b6
unknown <pavithrapavi.sightspectrum@gmail.com>
2023-09-13 04:24:20 -0700

    first commit

---
+++ loop_code/main.py
@@ -0,0 +1,52 @@
+counter=1
+while(counter<=5):
+    newcounter=1
+    while(newcounter<=5):
+        print("wellcome")
+        newcounter+=1
+    print("hello")
+    counter+=1
+counter=1
+while(counter<=10):
+   counter+=1
+   print("hiii")
+a="welcome"
+for i in range(1,21,2):
+    print(i)
+a=[1,2,3,4,5]
+list=int(input("enter a value:"))
+for i in a:
+    if(list==2):
+      print('you get the value 2')
+      break
+    elif(list==3):
+        print('you get the value 3')
+        break
+    elif(list==4):
+        print('you get the value 4')
+        break
+    elif(list==5):
+        print('you get the value 5')
+        break
+else:
+    print('the value is different')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
