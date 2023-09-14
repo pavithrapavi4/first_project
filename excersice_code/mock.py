814a64c2a8e9d738a647e9148741276f20b05fa1
unknown <pavithrapavi.sightspectrum@gmail.com>
2023-09-14 05:42:38 -0700

    first commit

---
+++ excersice_code/mock.py
@@ -0,0 +1,51 @@
+
+"""print(fruits)
+for i in fruits:
+    print(i)
+for i in fruits:
+    print(i)
+   if i=="orange":
+       break"""
+"""print(len(fruits))
+print(type(fruits))
+print(fruits)
+if 'kiwi' in fruits:
+ print("yes")
+fruits[2]="blackcurrent"
+print(fruits)
+fruits.insert(2,'watermelon')
+fruits.append('kiwi')
+list=['staberry','blueberry']
+fruits.extend(list)
+fruits.remove('staberry')
+fruits.pop()
+del fruits[2]
+fruits.clear()
+print(fruits)
+newlist=[]
+for x in fruits:
+ if 'a' in x:
+    newlist.append(x)
+print(newlist)"""
+"""l1=[1,2,3]
+l2=['a','b','c']
+l1.extend(l2)
+print(l1)"""
+"""var=int(input("enter the value:"))
+for x in var:
+ if x==8:
+  print("the value is 8")
+ elif x==4:
+    print("the value is 4")
+  elif x==6:
+    print("the value is 6")
+else:
+ print("It can be different value")"""
+var={True:"one",1:"two",1.0:"three"}
+print(var)
+
+
+
+
+
+
