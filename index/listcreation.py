5be487d13ed5638fdedd427a4a1415fd532cb6af
unknown <pavithrapavi.sightspectrum@gmail.com>
2023-09-19 05:46:52 -0700

    first commit

---
+++ index/listcreation.py
@@ -0,0 +1,19 @@
+"creation list"
+list=["apple","banana",4,"hellow"]
+list.append("loos")
+print(list)
+list= ["apple","banana","hellow","apple"]
+list.clear()
+print(list)
+
+var=[1,2,3,5,['a','b','c'],91]
+
+print(var[4][1:])
+list1=['a','b','c']
+list2=[1,2,3]
+list1.extend(list2)
+print(list1)
+
+
+
+
