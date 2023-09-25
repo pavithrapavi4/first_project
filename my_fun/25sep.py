8c58f253fc3d09c183aa7fd01ce86c1c5cb93534
unknown <pavithrapavi.sightspectrum@gmail.com>
2023-09-25 05:11:13 -0700

    first commit

---
+++ my_fun/25sep.py
@@ -0,0 +1,20 @@
+def funname(name="xyz"):
+    print("welcome"+name)
+funname("pavi")
+funname()
+def myfun(fname,lname):
+    print(fname +" "+lname)
+myfun("emil","gmail")
+def my_fun(**kwargs):
+    print(kwargs)
+    for i in kwargs:
+        print(i)
+my_fun(a=10,b=20,c=30,d=40)
+a=("string",["string","string"])
+for i,j in a:
+    print(i,j)
+    #print(j)
+
+
+
+
