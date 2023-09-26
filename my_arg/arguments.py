0655454c6603e3924520ebcee63b24c37c156eee
unknown <pavithrapavi.sightspectrum@gmail.com>
2023-09-26 04:59:02 -0700

    first commit

---
+++ my_arg/arguments.py
@@ -0,0 +1,18 @@
+def my_fun(name=" xyz"):
+    print("how are you"+name)
+my_fun(" pavi")
+my_fun(" sowmiya")
+my_fun(" sweetha")
+my_fun( )
+def my_fun(food):
+    for x in food:
+        print(x)
+fruits=["apple","banana","cherry"]
+my_fun(fruits)
+def my_fun(x):
+  return 5 * x
+print(my_fun(3))
+print(my_fun(6))
+print(my_fun(8))
+
+
